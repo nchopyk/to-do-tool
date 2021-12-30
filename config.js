1bb410a5c632e71b42077ddf3eebb44f8f520872
Nazar Chopyk <nazar.chopyk@gmail.com>
2021-12-31 00:24:28 +0200

    Added jwt key constants

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV, //development, staging, production
   PORT: process.env.PORT,
   HOST: process.env.HOST,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   DB_CONNECTION_PARAMS: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
