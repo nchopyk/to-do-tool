7a2b0f3c656eb9f57ea9a4eac083c1305db8265c
Nazar Chopyk <nazar.chopyk@gmail.com>
2021-12-31 09:13:43 +0200

    Added CLIENT_URL constant

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+
 module.exports = {
   NODE_ENV: process.env.NODE_ENV, //development, staging, production
   PORT: process.env.PORT,
   HOST: process.env.HOST,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  CLIENT_URL: process.env.CLIENT_URL,
   DB_CONNECTION_PARAMS: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
