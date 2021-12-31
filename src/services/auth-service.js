e012a2f655e74e70d5799fac7843078aa19d9d7b
Nazar Chopyk <nazar.chopyk@gmail.com>
2021-12-31 08:37:43 +0200

    Refactored dbQueries methods

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -9,13 +9,13 @@ class AuthService {
   }
 
   async signUp({ email, password, username }) {
-    const [existingUser] = await this.dbQueries.getUserByEmail(email);
+    const existingUser = await this.dbQueries.getUserByEmail(email);
 
     if (existingUser) throw new Error('User with such email already exists');
 
-    const hashPassword = await this.fastify.bcrypt.hash('password');
-    const [newUserId] = await this.dbQueries.insertNewUser({ email, password: hashPassword, username, activationLink: hashPassword });
-    const [user] = await this.dbQueries.getUserById(newUserId);
+    const hashPassword = await this.fastify.bcrypt.hash(password);
+    const newUserId = await this.dbQueries.insertNewUser({ email, password: hashPassword, username, activationLink: hashPassword });
+    const user = await this.dbQueries.getUserById(newUserId);
 
     await this.mailService.sendActivationMail({ to: email, link: hashPassword });
 
@@ -37,9 +37,18 @@ class AuthService {
   async refreshTokens() {}
 
   dbQueries = {
-    getUserByEmail: async (email) => this.dbClient.select().from('users').where('email', email),
-    getUserById: async (id) => this.dbClient.select().from('users').where('id', id),
-    insertNewUser: async ({ username, email, password }) => this.dbClient('users').insert({ username, email, password }),
+    getUserByEmail: async (email) => {
+      const [user] = this.dbClient.select().from('users').where({ email });
+      return user;
+    },
+    getUserById: async (id) => {
+      const [user] = this.dbClient.select().from('users').where({ id });
+      return user;
+    },
+    insertNewUser: async ({ username, email, password }) => {
+      const [newUserId] = this.dbClient('users').insert({ username, email, password });
+      return newUserId;
+    },
   };
 }
 
