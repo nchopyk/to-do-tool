6845f1df85f696c0afbbfa6bf1f09893311b4aef
Nazar Chopyk <nazar.chopyk@gmail.com>
2022-03-24 22:57:17 +0200

    Code refactoring

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const { build } = require('./app.js');
-const config = require('./config');
+const { PORT, HOST } = require('./config');
 
 const app = build({ logger: true });
 
-app.listen(config.PORT, config.HOST, (err, address) => {
+app.listen(PORT, HOST, (err) => {
   if (err) {
     console.log(err);
     process.exit(1);
